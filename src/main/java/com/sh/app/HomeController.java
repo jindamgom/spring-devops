39f3eff22d073e21d47495fc00d137c673b594ad
jindamgom <update_set@naver.com>
2024-02-05 14:25:45 +0900

    Feat : 0205 prod DB wallet 추가

---
+++ src/main/java/com/sh/app/HomeController.java
@@ -0,0 +1,23 @@
+package com.sh.app;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //간단한 컨트롤
+@Slf4j
+public class HomeController {
+
+    @Value("${profile.value}")
+    private String value;
+
+    @GetMapping("/")
+    public ResponseEntity<?> home()
+    {
+        return ResponseEntity.ok("현재 profile은 %s 입니다..".formatted(value));
+    }
+}
